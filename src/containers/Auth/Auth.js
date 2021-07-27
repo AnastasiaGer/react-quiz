4bd584c2f5a8216c7a24458dd7ee48062c28e3f6
AnastasiaGer <aagermanova@gmail.com>
2021-07-27 14:06:26 -0700

    renderInputs

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,36 +1,82 @@
 import React from "react";
 import classes from "./Auth.module.css";
-import Button from '../../components/UI/Button/Button'
-import Input from '../../components/UI/Input/Input'
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export default class Auth extends React.Component {
-  loginHandler =() => {
+  state = {
+    formControls: {
+      email: {
+        value: "",
+        type: "email",
+        label: "Email",
+        errorMessage: "Need correct Email",
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: "",
+        type: "password",
+        label: "Password",
+        errorMessage: "Need correct password",
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
+  loginHandler = () => {};
 
-  }
-
-  registerHandler =() => {
-    
-  }
+  registerHandler = () => {};
 
   submitHandler(e) {
-    e.preventDefault()
+    e.preventDefault();
+  }
+  onChangeHandler = (event, controlName) => {
+console.log(`${controlName}: `, event.target.value);
+
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          errorMessage={control.errorMessage}
+          shouldValidate={!!true}
+          onChange={(event) => this.onChangeHandler(event, controlName)}
+        />
+      );
+    });
   }
 
   render() {
     return (
       <div className={classes.Auth}>
         <div>
-        <h1>Auth</h1>
-        <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-          <Input 
-          label='Email'/>
-
-          <Input 
-          label='Password' errorMessage='' />
-
-          <Button type='success' onClick={this.loginHandler}>Log In</Button>
-          <Button type='primary' onClick={this.registerHandler}>Sing In</Button>
-        </form>
+          <h1>Auth</h1>
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            {this.renderInputs()}
+            <Button type="success" onClick={this.loginHandler}>
+              Log In
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Sing In
+            </Button>
+          </form>
         </div>
       </div>
     );
