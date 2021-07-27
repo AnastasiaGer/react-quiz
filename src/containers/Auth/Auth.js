b0d1620315d07b99e187d4cb87555c72863ece2f
AnastasiaGer <aagermanova@gmail.com>
2021-07-27 14:17:52 -0700

    is_js

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,52 +1,92 @@
-import React from "react";
-import classes from "./Auth.module.css";
-import Button from "../../components/UI/Button/Button";
-import Input from "../../components/UI/Input/Input";
+import React, {Component} from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
+import is from 'is_js'
 
-export default class Auth extends React.Component {
+export default class Auth extends Component {
+
   state = {
     formControls: {
       email: {
-        value: "",
-        type: "email",
-        label: "Email",
-        errorMessage: "Need correct Email",
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
         valid: false,
         touched: false,
         validation: {
           required: true,
-          email: true,
-        },
+          email: true
+        }
       },
       password: {
-        value: "",
-        type: "password",
-        label: "Password",
-        errorMessage: "Need correct password",
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
         valid: false,
         touched: false,
         validation: {
           required: true,
-          minLength: 6,
-        },
-      },
-    },
-  };
-  loginHandler = () => {};
-
-  registerHandler = () => {};
-
-  submitHandler(e) {
-    e.preventDefault();
+          minLength: 6
+        }
+      }
+    }
   }
-  onChangeHandler = (event, controlName) => {
-console.log(`${controlName}: `, event.target.value);
 
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
+  validateControl(value, validation) {
+    if (!validation) {
+      return true
+    }
+
+    let isValid = true
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if (validation.email) {
+      isValid = is.email(value) && isValid
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
+    }
+
+    return isValid
+  }
+
+  onChangeHandler = (event, controlName) => {
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
+
+    control.value = event.target.value
+    control.touched = true
+    control.valid = this.validateControl(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls
+    })
   }
 
   renderInputs() {
     return Object.keys(this.state.formControls).map((controlName, index) => {
-      const control = this.state.formControls[controlName];
+      const control = this.state.formControls[controlName]
       return (
         <Input
           key={controlName + index}
@@ -55,30 +95,40 @@ console.log(`${controlName}: `, event.target.value);
           valid={control.valid}
           touched={control.touched}
           label={control.label}
+          shouldValidate={!!control.validation}
           errorMessage={control.errorMessage}
-          shouldValidate={!!true}
-          onChange={(event) => this.onChangeHandler(event, controlName)}
+          onChange={event => this.onChangeHandler(event, controlName)}
         />
-      );
-    });
+      )
+    })
   }
 
   render() {
     return (
       <div className={classes.Auth}>
         <div>
-          <h1>Auth</h1>
+          <h1>Авторизация</h1>
+
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            {this.renderInputs()}
-            <Button type="success" onClick={this.loginHandler}>
-              Log In
+
+            { this.renderInputs() }
+
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Войти
             </Button>
-            <Button type="primary" onClick={this.registerHandler}>
-              Sing In
+
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Зарегистрироваться
             </Button>
           </form>
         </div>
       </div>
-    );
+    )
   }
 }
