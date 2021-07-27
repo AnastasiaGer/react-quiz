612e0d6a96754706fe9707fc963bc1982bdd1da3
AnastasiaGer <aagermanova@gmail.com>
2021-07-27 12:56:12 -0700

    Input

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./Auth.module.css";
 import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends React.Component {
   loginHandler =() => {
@@ -21,11 +22,13 @@ export default class Auth extends React.Component {
         <div>
         <h1>Auth</h1>
         <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-          <input type='text'/>
-          <input type='text'/>
+          <Input 
+          label='Email'/>
+
+          <Input 
+          label='Password' errorMessage='' />
 
           <Button type='success' onClick={this.loginHandler}>Log In</Button>
-
           <Button type='primary' onClick={this.registerHandler}>Sing In</Button>
         </form>
         </div>
