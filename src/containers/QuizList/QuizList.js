65b293a0d086ff9127b1b062e227be9669c127f9
AnastasiaGer <aagermanova@gmail.com>
2021-07-27 18:25:13 -0700

    axios

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import classes from './QuizList.module.css'
 import {NavLink} from 'react-router-dom'
-
+import axios from 'axios'
 export default class QuizList extends Component {
 
   renderQuizes() {
@@ -16,6 +16,14 @@ export default class QuizList extends Component {
         </li>
       )
     })
+  }
+
+  componentDidMount() {
+    axios.get('https://react-quiz-f5514-default-rtdb.firebaseio.com/quiz.json')
+    .then(response => {
+      console.log(response);
+    }
+    )
   }
 
   render() {
