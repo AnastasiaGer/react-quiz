c5616a24773a752ce2d6100f48c2545940937fa3
AnastasiaGer <aagermanova@gmail.com>
2021-07-28 19:55:09 -0700

    redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import {BrowserRouter} from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducers from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const store = createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
-ReactDOM.render(app, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(app, document.getElementById("root"));
+registerServiceWorker();
