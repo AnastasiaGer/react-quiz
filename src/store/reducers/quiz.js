5e2c2fb0b1082bfc6449cb0f907622231f2b0c62
AnastasiaGer <aagermanova@gmail.com>
2021-07-29 19:03:44 -0700

    Quiz-2

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -3,6 +3,10 @@ import {
   FETCH_QUIZES_SUCCESS,
   FETCH_QUIZES_ERROR,
   FETCH_QUIZ_SUCCESS,
+  QUIZ_SET_STATE,
+  FINISH_QUIZ,
+  QUIZ_NEXT_QUESTION,
+  RETRY_QUIZ,
 } from "../actions/actionTypes";
 
 const initialState = {
@@ -35,12 +39,37 @@ export default function quizReducer(state = initialState, action) {
         loading: false,
         error: action.error,
       };
-      case FETCH_QUIZ_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          quiz: action.quiz,
-        };
+    case FETCH_QUIZ_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        quiz: action.quiz,
+      };
+    case QUIZ_SET_STATE:
+      return {
+        ...state,
+        answerState: action.answerState,
+        results: action.results,
+      };
+    case FINISH_QUIZ:
+      return {
+        ...state,
+        isFinished: true,
+      };
+    case QUIZ_NEXT_QUESTION:
+      return {
+        ...state,
+        activeQuestion: action.number,
+        answerState: null,
+      };
+    case RETRY_QUIZ:
+      return {
+        ...state,
+        activeQuestion: 0,
+        answerState: null,
+        isFinished: false,
+        results: {},
+      };
     default:
       return state;
   }
